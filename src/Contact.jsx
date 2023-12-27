4a6303887d4f8b35846e176b8c575cfb80b205cf
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-27 22:28:49 +0430

    set up

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -6,8 +6,5 @@ export default function Contact(props) {
   return (
     <div className="contact-card">
       <img src={props.img} />
-      <h3>{props.name}</h3>
-      <div className="info-group">
-        <img src={PhoneIcone} />
   );
 }
