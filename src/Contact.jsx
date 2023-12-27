236bfac1c6801d9b846ce07fd3839b05a2abeff3
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-27 22:28:57 +0430

    set up

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,8 +3,3 @@ import MailIcon from "./images/mail-icon.png";
 import PhoneIcone from "./images/phone-icon.png";
 
 export default function Contact(props) {
-  return (
-    <div className="contact-card">
-      <img src={props.img} />
-  );
-}
