db647a3a79f3371aaa777d0315367241d7a46ca0
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-28 07:53:00 +0430

    set up

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ function App() {
   return (
     <div className="contacts">
       <Contact
-        img={Pumpkin}
         name="Pumpkin"
         phone="(0800) CAT KING"
         email="pumpkin@scrimba.com"
