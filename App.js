2bf4353900154595331486be774792a9e6ee84ad
MastooraTurkmen <janmastoorajan@gmail.com>
2024-01-07 09:44:54 +0430

    set up

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+      </header>
     </div>
   );
 }
